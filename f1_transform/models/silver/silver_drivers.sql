8f9970dfaa4858aa3d09af060972ab6de484f460
Remy Queny <remy.queny@etu.isima.fr>
2026-07-09 17:08:56 +0200

    add silver_drivers.sql (nettoyage)

---
+++ f1_transform/models/silver/silver_drivers.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized='external',
+    location='azure://silver/drivers/silver_drivers.parquet'
+) }}
+
+WITH source_data AS (
+    SELECT *
+    FROM 'azure://bronze/drivers/drivers_latest.parquet'
+)
+
+SELECT
+    CAST(driver_number AS INTEGER) AS numero_pilote,
+    CAST(full_name AS VARCHAR) AS nom_complet,
+    CAST(country_code AS INTEGER) AS code_pays,
+    UPPER(last_name) AS nom_pilote,
+FROM source_data
+WHERE driver_number IS NOT NULL
