0450464d81ffa14144cbb56b4516c6a76185368f
Remy Queny <remy.queny@etu.isima.fr>
2026-07-13 12:09:55 +0200

    update gold_drivers_by_country

--- f1_transform/models/gold/gold_drivers_by_country.sql
+++ f1_transform/models/gold/gold_drivers_by_country.sql
@@ -12,4 +12,4 @@ SELECT code_pays, COUNT(numero_pilote) AS nombre_de_pilotes
 FROM silver_data
 WHERE code_pays IS NOT NULL
 GROUP BY code_pays
-ORDER BY nombre_de_pilotes DESC;
+ORDER BY nombre_de_pilotes DESC
